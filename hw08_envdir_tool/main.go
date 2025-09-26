c9e7db1b28a234b255ba4f972fed19d11b50256b
fawwns <elizaveta.kyz@mail.ru>
2025-09-26 19:39:25 +0700

    HW8 is completed/2

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <envdir> <command> [args...]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	envDir := os.Args[1]
+	cmd := os.Args[2:]
+
+	envs, err := ReadDir(envDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading envdir: %v\n", err)
+		os.Exit(1)
+	}
+
+	code := RunCmd(cmd, envs)
+	os.Exit(code)
 }
