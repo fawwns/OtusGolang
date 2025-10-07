80129b2c8c47d9037fc9db9fd3e3886f96248452
fawwns <elizaveta.kyz@mail.ru>
2025-10-07 22:33:15 +0700

    HW10 is completed

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,11 +1,12 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
-	"fmt"
+	"bufio"
 	"io"
 	"regexp"
 	"strings"
+
+	"github.com/buger/jsonparser"
 )
 
 type User struct {
@@ -21,46 +22,26 @@ type User struct {
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
-
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
-		}
-		result[i] = user
-	}
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
+	re := regexp.MustCompile("\\." + domain + "$")
 
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+
+		email, err := jsonparser.GetString(line, "Email")
 		if err != nil {
-			return nil, err
+			continue
+		}
+		if re.MatchString(email) {
+			domainPart := strings.ToLower(strings.SplitN(email, "@", 2)[1])
+			result[domainPart]++
 		}
 
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
-		}
 	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
 	return result, nil
 }
