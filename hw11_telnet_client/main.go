ea9b869914c0681aa5c601b4d8a17bacceea54c1
fawwns <elizaveta.kyz@mail.ru>
2025-10-12 21:07:18 +0700

    HW11 is completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,35 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", 10*time.Second, "connection timeout")
+	flag.Parse()
+
+	if flag.NArg() < 2 {
+		println("Usage: go-telnet [--timeout=10s] host port")
+		return
+	}
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+	address := net.JoinHostPort(host, port)
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		<-ctx.Done()
+		os.Exit(0)
+	}()
+
+	NewTelnetClient(address, *timeout, os.Stdin, os.Stdout)
 }
